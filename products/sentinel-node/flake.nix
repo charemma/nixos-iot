436cc515c4f73360e602f6132437f7e8d59758a7
Charalambos Emmanouilidis <charalambos.emmanouilidis@proton.me>
2026-03-18 18:31:38 +0200

    refactor(sentinel-node): import platform modules explicitly, skip core.nix

--- products/sentinel-node/flake.nix
+++ products/sentinel-node/flake.nix
@@ -12,7 +12,13 @@
   outputs = { self, nixpkgs, platform, sentinel, ... }: {
     nixosConfigurations.sentinel-node = nixpkgs.lib.nixosSystem {
       system = "aarch64-linux";
-      modules = builtins.attrValues platform.nixosModules ++ [
+      modules = [
+        platform.nixosModules.raspberry-pi
+        platform.nixosModules.sd-image
+        platform.nixosModules.bsp-rpi
+        platform.nixosModules.base
+        platform.nixosModules.user
+        platform.nixosModules.authorized-keys
         sentinel.nixosModules.default
         ./configuration.nix
       ];
